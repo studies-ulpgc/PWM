bbadacc7139ea597f893138fe55721f8392e0019
SaturnAco <h.wrks097@gmail.com>
2026-02-23 16:46:28 +0100

    Cambio de nombre de ver_cesta a calculo_precio

--- js/calculo_precio.js
+++ js/calculo_precio.js
@@ -72,35 +72,4 @@ function renderCart() {
   updateTotal();
 }
 
-async function cargarHeader() {
-  try {
-    const response = await fetch('HEADER_CORTO.html');
-    const templateHTML = await response.text();
-    
-    const contenedor = document.getElementById('header');
-    contenedor.innerHTML = templateHTML;
-
-    const grande = contenedor.querySelector('.default-GRANDE');
-    if (grande) grande.remove();
-
-  } catch (error) {
-    console.error('Error cargando header:', error);
-  }
-}
-
-async function cargarFooter() {
-  try {
-    const response = await fetch('FOOTER.html');
-    const templateHTML = await response.text();
-    
-    const contenedor = document.getElementById('footer');
-    contenedor.innerHTML = templateHTML;
-
-  } catch (error) {
-    console.error('Error cargando footer:', error);
-  }
-}
-
 renderCart();
-cargarHeader();
-cargarFooter();
