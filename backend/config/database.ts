74e3f77a5020a24e3838914311c34db94718a172
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-03-09 23:32:48 +0000

    Cosas añadidas, experimentando

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import type { Core } from '@strapi/strapi';
 
 const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Database => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     mysql: {
