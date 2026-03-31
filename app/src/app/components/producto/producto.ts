9328c42231e5f275803a51d69b1c1a6014b5e62e
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-01 00:35:13 +0100

    Funciona producto más o menos

--- app/src/app/components/producto/producto.ts
+++ app/src/app/components/producto/producto.ts
@@ -1,13 +1,27 @@
-import { Component, Input } from '@angular/core'; // <--- Importante: Input
-import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Para el pipe de decimales o si usas @if
+import { RouterLink } from '@angular/router'; // <--- ESTA ES LA CLAVE
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [CommonModule],
+  // Aquí es donde le damos permiso al HTML para usar routerLink
+  imports: [CommonModule, RouterLink], 
   templateUrl: './producto.html',
-  styleUrl: './producto.css'
+  styleUrls: ['./producto.css']
 })
 export class Producto {
-  @Input() data: any; // <--- ESTO ES LO QUE FALTA
+  @Input() data: any;
+
+  // No olvides declarar también las funciones que pusimos en el HTML
+  // para que no te de el error de "Property does not exist"
+  agregarAlCarrito(event: Event) {
+    event.stopPropagation(); // Evita que al hacer clic se abra el producto
+    console.log('Añadido al carrito:', this.data?.nombre);
+  }
+
+  agregarADeseados(event: Event) {
+    event.stopPropagation();
+    console.log('Añadido a deseados:', this.data?.nombre);
+  }
 }
