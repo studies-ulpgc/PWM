bbc5acd0cbeac0f1925637187b4fee4aaaae1796
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-01 13:38:07 +0100

    Arreglado del centralizado

---
+++ app/src/app/components/desplegable-header/desplegable-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-desplegable-header',
+  imports: [],
+  templateUrl: './desplegable-header.html',
+  styleUrl: './desplegable-header.css',
+})
+export class DesplegableHeader {}
