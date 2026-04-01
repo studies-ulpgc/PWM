bbacce998546ca767597b4c2e4af31acf530021f
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-01 01:22:15 +0100

    Funciona producto

--- app/src/app/components/producto/producto.ts
+++ app/src/app/components/producto/producto.ts
@@ -5,7 +5,6 @@ import { RouterLink } from '@angular/router'; // <--- ESTA ES LA CLAVE
 @Component({
   selector: 'app-producto',
   standalone: true,
-  // Aquí es donde le damos permiso al HTML para usar routerLink
   imports: [CommonModule, RouterLink], 
   templateUrl: './producto.html',
   styleUrls: ['./producto.css']
@@ -13,10 +12,8 @@ import { RouterLink } from '@angular/router'; // <--- ESTA ES LA CLAVE
 export class Producto {
   @Input() data: any;
 
-  // No olvides declarar también las funciones que pusimos en el HTML
-  // para que no te de el error de "Property does not exist"
   agregarAlCarrito(event: Event) {
-    event.stopPropagation(); // Evita que al hacer clic se abra el producto
+    event.stopPropagation(); 
     console.log('Añadido al carrito:', this.data?.nombre);
   }
 
