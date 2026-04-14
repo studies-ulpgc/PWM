28a730d4ff7f79cd97195143e356aca754208f2f
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-04-14 21:45:20 +0100

    Producto se ve en grisaceo, pero hay problemas con que hay que darle varias veces

--- app/src/app/components/producto/producto.ts
+++ app/src/app/components/producto/producto.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AutentificacionService } from '../../services/autentificacion.service'; // Ajusta la ruta
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-producto',
@@ -9,8 +11,22 @@ import { RouterLink } from '@angular/router';
   templateUrl: './producto.html',
   styleUrls: ['./producto.css']
 })
-export class Producto {
+export class Producto implements OnInit, OnDestroy {
   @Input() data: any;
+  isLoggedIn = false;
+  private authSub?: Subscription;
+
+  constructor(private authService: AutentificacionService) {}
+
+  ngOnInit() {
+    this.authSub = this.authService.user$.subscribe(user => {
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  ngOnDestroy() {
+    this.authSub?.unsubscribe();
+  }
 
   obtenerRating(): number {
     if (!this.data?.Valoracion || this.data.Valoracion.length === 0) return 0;
