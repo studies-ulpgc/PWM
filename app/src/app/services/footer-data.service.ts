b48ec0dfea27dfebe8e6b09d3ff4dcd624f46df5
AliciaM05 <aliciarotru@gmail.com>
2026-04-20 16:24:17 +0100

    Cambio en la forma de cargar idiomas y localización

--- app/src/app/services/footer-data.service.ts
+++ app/src/app/services/footer-data.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { Observable, from, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FooterDataService {
-  constructor(private http: HttpClient) {}
+  constructor(private firestore: Firestore) {}
 
   getIdiomas(): Observable<any[]> {
-    return this.http.get<any>('assets/json/idiomas.json').pipe(
-      map(res => res.data || [])
+    const idiomasRef = collection(this.firestore, 'idiomas');
+    return from(getDocs(idiomasRef)).pipe(
+      map(snapshot => snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
     );
   }
 
   getInternacional(): Observable<any[]> {
-    return this.http.get<any>('assets/json/internacional.json').pipe(
-      map(res => res.data || [])
+    const interRef = collection(this.firestore, 'internacional');
+    return from(getDocs(interRef)).pipe(
+      map(snapshot => snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })))
     );
   }
 }
