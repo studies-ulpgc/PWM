f2be474b3c2e0e96ed20d4785597a6dbabab1c13
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-05-07 00:08:16 +0100

    Producto está hecho

--- app/src/app/components/producto/producto.component.ts
+++ app/src/app/components/producto/producto.component.ts
@@ -1,54 +1,58 @@
 import { Component, Input, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { IonCard, IonCardTitle, IonButton, IonIcon, IonCardSubtitle } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { bagAddOutline, bagCheck, heart, heartOutline } from 'ionicons/icons';
 import { AutentificacionService } from '../../services/autentificacion.service';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-producto',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [IonCardSubtitle, 
+    CommonModule, 
+    RouterModule, 
+    IonCard, 
+    IonCardTitle, 
+    IonButton, 
+    IonIcon
+  ],
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent implements OnInit, OnDestroy {
   @Input() data: any;
   isLoggedIn = false;
-  enCesta: boolean = false;
-  enDeseados: boolean = false;
-
+  enCesta = false;
+  enDeseados = false;
   private authSub?: Subscription;
 
-  constructor(private authService: AutentificacionService, private cdr: ChangeDetectorRef) {}
+  constructor(
+    private authService: AutentificacionService, 
+    private cdr: ChangeDetectorRef
+  ) {
+    // Registramos los iconos de Ionic
+    addIcons({ bagAddOutline, bagCheck, heart, heartOutline });
+  }
 
   ngOnInit() {
     this.authSub = this.authService.user$.subscribe(user => {
       this.isLoggedIn = !!user;
-
       if (!this.isLoggedIn) {
         this.enCesta = false;
         this.enDeseados = false;
       }
-
-      this.cdr.markForCheck();
       this.cdr.detectChanges();
     });
   }
 
   toggleCesta() {
-    if (this.isLoggedIn) {
-      this.enCesta = !this.enCesta;
-    }
+    if (this.isLoggedIn) this.enCesta = !this.enCesta;
   }
 
   toggleDeseados() {
-    if (this.isLoggedIn) {
-      this.enDeseados = !this.enDeseados;
-    }
-  }
-
-  ngOnDestroy() {
-    this.authSub?.unsubscribe();
+    if (this.isLoggedIn) this.enDeseados = !this.enDeseados;
   }
 
   obtenerRating(): number {
@@ -56,5 +60,9 @@ export class ProductoComponent implements OnInit, OnDestroy {
     const nombreArchivo = this.data.Valoracion[0].name;
     const rating = parseInt(nombreArchivo.split('_')[0]);
     return isNaN(rating) ? 0 : rating;
+  }
+
+  ngOnDestroy() {
+    this.authSub?.unsubscribe();
   }
 }
