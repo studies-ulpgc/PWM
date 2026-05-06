57890f449ecbc52ec1f5af84920ff4dde2f95011
studies-ulpgc <dacil.santana108@alu.ulpgc.es>
2026-05-06 21:48:57 +0100

    Home hecho, trabajando en que funcione en android

--- app/src/app/pages/home/home.component.ts
+++ app/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+// Importa los componentes de Ionic Standalone
+import { IonContent, IonHeader } from '@ionic/angular/standalone'; 
 import { HeaderGrandeComponent } from '../../components/header-grande/header-grande.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { ProductoComponent } from '../../components/producto/producto.component';
@@ -9,7 +11,8 @@ import { ProductoService } from '../../services/producto.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderGrandeComponent, FooterComponent, ProductoComponent],
+  // Añade IonContent aquí
+  imports: [CommonModule, IonHeader, RouterModule, IonContent, HeaderGrandeComponent, FooterComponent, ProductoComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
