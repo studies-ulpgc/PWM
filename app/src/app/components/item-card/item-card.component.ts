c617b9ae70759a8cde84daf262dcf3d512f11996
SaturnAco <h.wrks097@gmail.com>
2026-05-10 20:19:48 +0200

    item-card con ionic

--- app/src/app/components/item-card/item-card.component.ts
+++ app/src/app/components/item-card/item-card.component.ts
@@ -1,18 +1,26 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartItem } from '../../pages/ver-cesta/cart-item.model.component';
+// Importaciones de Ionic
+import { IonCheckbox, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { trashOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-item-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonCheckbox, IonButton, IonIcon],
   templateUrl: './item-card.component.html',
   styleUrls: ['./item-card.component.css']
 })
 export class ItemCardComponent {
   @Input() item!: CartItem;
   @Output() selectionChange = new EventEmitter<void>();
-  @Output() deleteItem = new EventEmitter<string | number>(); // Nuevo evento
+  @Output() deleteItem = new EventEmitter<string | number>();
+
+  constructor() {
+    addIcons({ trashOutline });
+  }
 
   onToggle() {
     this.item.selected = !this.item.selected;
@@ -20,6 +28,6 @@ export class ItemCardComponent {
   }
 
   onDelete() {
-    this.deleteItem.emit(this.item.id); // Avisamos que queremos borrarlo
+    this.deleteItem.emit(this.item.id);
   }
 }
