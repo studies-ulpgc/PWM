5b556ce12739ad467493c080ff5964a25e94005f
SaturnAco <h.wrks097@gmail.com>
2026-05-10 21:20:02 +0200

    item-comprad con ionic

--- app/src/app/components/item-comprado/item-comprado.component.ts
+++ app/src/app/components/item-comprado/item-comprado.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonCard, IonCheckbox } from '@ionic/angular/standalone';
 import { OrderItem } from '../../pages/lista-pedidos-realizados/order-item.model.component';
 
 @Component({
   selector: 'app-item-comprado',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, IonCard, IonCheckbox],
   templateUrl: './item-comprado.component.html',
   styleUrls: ['./item-comprado.component.css']
 })
