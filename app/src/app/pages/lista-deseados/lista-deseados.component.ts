9d645f7d1ace92e81224636104dfb9f8e2c17fdf
SaturnAco <h.wrks097@gmail.com>
2026-05-10 20:49:11 +0200

    lista deseados con ionic

--- app/src/app/pages/lista-deseados/lista-deseados.component.ts
+++ app/src/app/pages/lista-deseados/lista-deseados.component.ts
@@ -1,21 +1,26 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { 
+  IonContent, IonGrid, IonRow, IonCol, IonHeader 
+} from '@ionic/angular/standalone';
 import { HeaderGrandeComponent } from '../../components/header-grande/header-grande.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { SimilaresComponent } from '../../components/similares/similares.component';
 import { ItemWantedComponent } from '../../components/item-wanted/item-wanted.component';
-import { ProductoService } from '../../services/producto.service';
 import { WantedItem } from './wanted-item.model.component';
 import { DatabaseService } from '../../services/database.service';
 
 @Component({
   selector: 'app-lista-deseados',
   standalone: true,
-  imports: [CommonModule, HeaderGrandeComponent, FooterComponent, SimilaresComponent, ItemWantedComponent],
+  imports: [
+    CommonModule, 
+    IonContent, IonGrid, IonRow, IonCol, IonHeader,
+    HeaderGrandeComponent, FooterComponent, SimilaresComponent, ItemWantedComponent
+  ],
   templateUrl: './lista-deseados.component.html',
   styleUrls: ['./lista-deseados.component.css']
 })
-
 export class ListaDeseadosComponent implements OnInit {
   wantedItems: WantedItem[] = [];
 
@@ -31,9 +36,7 @@ export class ListaDeseadosComponent implements OnInit {
   async cargarDeseados() {
     const itemsLocal = await this.dbService.getDeseados();
     
-    // Usamos Promise.all porque vamos a hacer una consulta asíncrona (exists) por cada item
     this.wantedItems = await Promise.all(itemsLocal.map(async (item: any) => {
-      // Comprobamos si este item de deseados está en la cesta
       const estaEnCesta = await this.dbService.exists('cesta', item.id.toString());
       
       return {
@@ -41,7 +44,7 @@ export class ListaDeseadosComponent implements OnInit {
         name: item.nombre,
         price: item.precio,
         img: item.img,
-        added: estaEnCesta, // Si está en la cesta, el botón dirá "Añadido"
+        added: estaEnCesta,
         opts: 'Talla única'
       };
     }));
@@ -51,6 +54,6 @@ export class ListaDeseadosComponent implements OnInit {
 
   async eliminarDeDeseados(id: string | number) {
     await this.dbService.removeDeseado(id);
-    await this.cargarDeseados(); // Refrescamos la lista
+    await this.cargarDeseados();
   }
 }
