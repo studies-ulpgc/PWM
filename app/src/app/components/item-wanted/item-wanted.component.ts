187e1ef6552b6c6fb8ca4c3068f0ce130f0c441b
SaturnAco <h.wrks097@gmail.com>
2026-05-10 20:48:57 +0200

    item wanted con ionic

--- app/src/app/components/item-wanted/item-wanted.component.ts
+++ app/src/app/components/item-wanted/item-wanted.component.ts
@@ -1,39 +1,44 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { DatabaseService } from '../../services/database.service';
 import { CommonModule } from '@angular/common';
+import { 
+  IonCard, IonButton, IonIcon, IonLabel, IonImg 
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline, cartOutline, checkmarkOutline } from 'ionicons/icons';
+import { DatabaseService } from '../../services/database.service';
 import { WantedItem } from '../../pages/lista-deseados/wanted-item.model.component';
 
 @Component({
   selector: 'app-item-wanted',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, IonCard, IonButton, IonIcon, IonLabel, IonImg],
   templateUrl: './item-wanted.component.html',
   styleUrls: ['./item-wanted.component.css']
 })
 export class ItemWantedComponent {
   @Input() item!: WantedItem;
-  @Output() remove = new EventEmitter<void>(); // Evento para borrar de la lista
+  @Output() remove = new EventEmitter<void>();
 
-  constructor(private dbService: DatabaseService) {}
+  constructor(private dbService: DatabaseService) {
+    addIcons({ closeOutline, cartOutline, checkmarkOutline });
+  }
 
   async toggleAdd() {
     this.item.added = !this.item.added;
 
     if (this.item.added) {
-      // Si el botón cambia a "Añadido", lo guardamos en la tabla cesta
       await this.dbService.addCesta({
         id: this.item.id,
         nombre: this.item.name,
         precio: this.item.price,
-        fotoUrl: this.item.img
+        img: this.item.img // Asegúrate de que el campo sea 'img' como en tu modelo
       });
     } else {
-      // Si lo desmarcamos, lo quitamos de la cesta
       await this.dbService.removeCesta(this.item.id);
     }
   }
 
   onRemove() {
-    this.remove.emit(); // Avisamos al padre para que ejecute el DELETE
+    this.remove.emit();
   }
 }
